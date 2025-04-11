4ff935d960b195804e3db8e1429c0e162106e86d
Atabek_023_ <atabekquanaliyevvv@gmail.com>
2025-04-11 17:38:45 +0300

    connected database
    ,

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+class Database
+{
+    protected $host = "localhost";
+    protected $dbname = "mvc_tuto";
+    protected $dbport = "3306";
+    protected $username = "root";
+    protected $password = "";
+    public $conn;
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname;port=$this->dbport", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database Connection Failed: " . $e->getMessage()); // Завершает скрипт при ошибке
+        }
+        return $this->conn;
+    }
+    
+}
